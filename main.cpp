85ddf9434f8d75cd728f5450a34c5ba787c0346e
Araoluwa Akinyemi <arakinyemi@gmail.com>
2022-04-19 15:55:15 +0100

    To remove duplicate numbers

--- main.cpp
+++ main.cpp
@@ -1,42 +1,20 @@
 #include <bits/stdc++.h>
-
+#include <set>
 using namespace std;
 
-
-vector<int>adj[1000];
-vector<bool>visited(1000);
-int n;
-int co = 0;
-void dfs(int s){
-    if(visited[s]) return ;
-    visited[s] = true;
-    for(auto u : adj[s]){
-        dfs(u);
-    }
-}
-
-int findComponents (){
-    for(int i = 1; i <= n; i++){
-            if(!visited[i]){
-                co++;
-                dfs(i);
-        }
-    }
-    return co;
-}
 int main()
 {
-    int m, u, v, start, comp;
-    cout<<"Enter the number of nodes: ";
-    cin >> n;
-    cout<<"Enter the number of edges: ";
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        cout<<"Enter nodes for edge #"<<i+1<<": ";
-        cin >> u >> v;
-        adj[u].push_back(v);
-    }
-    comp = findComponents();
-    cout<<comp;
+   set<int>d;
+   int n, elem;
+   cout<<"How many numbers? ";
+   cin >> n;
+   cout<<"The numbers \n";
+   for(int i = 0; i < n; i++){
+    cin >> elem;
+    d.insert(elem);
+   }
+   for(auto u : d){
+    cout << u << " ";
+   }
     return 0;
 }
